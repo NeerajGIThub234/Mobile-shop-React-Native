64500fdf9b9ad2bd808de61117be20217952e222
NeerajGIThub234 <neeraj.20cse379@rtu.ac.in>
2024-10-14 17:11:58 +0530

    Initial commit

---
+++ src/data/constants.ts
@@ -0,0 +1,150 @@
+export const PRODUCTS_LIST: Product[] = [
+    {
+      "id": "10",
+      "name": "Xiaomi Redmi Note 9 (Polar White, 128 GB)",
+      "imageUrl": "https://rukminim2.flixcart.com/image/850/1000/kovsvbk0/mobile/t/j/o/note-9-m2003j15si-mzb9369in-redmi-original-imag3862mqzvd8yd.jpeg?q=90&crop=false",
+      "originalPrice": 15999,
+      "discountPrice": 13999,
+      "offerPercentage": 12,
+      "rating": 4.4,
+      "ratingCount": 8765,
+      "tags": [
+        "48MP Quad Rear Camera",
+        "13MP Front Camera",
+        "16.59 cm (6.53 inch) Full HD+ Display"
+      ]
+    },
+    {
+      id: '1',
+      name: 'APPLE iPhone 14 (Blue, 128 GB)',
+      imageUrl:
+        'https://rukminim1.flixcart.com/image/300/400/xif0q/mobile/3/5/l/-original-imaghx9qmgqsk9s4.jpeg',
+      originalPrice: 79990,
+      discountPrice: 65999,
+      offerPercentage: 17,
+      rating: 4.7,
+      ratingCount: 8794,
+      tags: [
+        '12MP Front Camera',
+        '12MP Dual Rear Camera',
+        '15.49 cm (6.1 inch) Super Retina XDR Display',
+      ],
+    },
+    {
+      id: '2',
+      name: 'APPLE iPhone 14 (Starlight, 256 GB)',
+      imageUrl:
+        'https://rukminim1.flixcart.com/image/300/400/xif0q/mobile/m/o/b/-original-imaghx9qkugtbfrn.jpeg',
+      originalPrice: 89900,
+      discountPrice: 75999,
+      offerPercentage: 15,
+      rating: 4.7,
+      ratingCount: 8794,
+      tags: ['12MP Front Camera', '15.49 cm (6.1 inch) Super Retina XDR Display'],
+    },
+    {
+      id: '3',
+      name: 'APPLE iPhone 14 (Purple, 128 GB)',
+      imageUrl:
+        'https://rukminim1.flixcart.com/image/300/400/xif0q/mobile/b/u/f/-original-imaghxa5hvapbfds.jpeg',
+      originalPrice: 79990,
+      discountPrice: 66999,
+      offerPercentage: 16,
+      rating: 4.7,
+      ratingCount: 8794,
+      tags: ['12MP Dual Rear Camera', '15.49 cm Super Retina XDR Display'],
+    },
+    {
+      id: '4',
+      name: 'APPLE iPhone 11 (White, 64 GB)',
+      imageUrl:
+        'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+      originalPrice: 43900,
+      discountPrice: 38999,
+      offerPercentage: 11,
+      rating: 4.6,
+      ratingCount: 180810,
+      tags: [
+        '12MP Front Camera',
+        '12MP Dual Rear Camera',
+        '15.49 cm (6.1 inch) Super Retina XDR Display',
+      ],
+    },
+      {
+        "id": "5",
+        "name": "Samsung Galaxy S23 Ultra (Phantom Black, 256 GB)",
+        "imageUrl": "https://m.media-amazon.com/images/I/71goZuIha-L._AC_UF1000,1000_QL80_.jpg",
+        "originalPrice": 124999,
+        "discountPrice": 109999,
+        "offerPercentage": 12,
+        "rating": 4.8,
+        "ratingCount": 6290,
+        "tags": [
+          "200MP Quad Rear Camera",
+          "12MP Front Camera",
+          "17.17 cm (6.8 inch) Dynamic AMOLED 2X Display"
+        ]
+      },
+      {
+        "id": "6",
+        "name": "OnePlus 11T (Jupiter Rock, 256 GB)",
+        "imageUrl": "https://cdn1.smartprix.com/rx-imnuwrjy0-w1200-h1200/mnuwrjy0.jpg",
+        "originalPrice": 59999,
+        "discountPrice": 49999,
+        "offerPercentage": 17,
+        "rating": 4.6,
+        "ratingCount": 5213,
+        "tags": [
+          "50MP Triple Rear Camera",
+          "16MP Front Camera",
+          "16.3 cm (6.7 inch) Fluid AMOLED Display"
+        ]
+      },
+      {
+        "id": "7",
+        "name": "Xiaomi Redmi Note 12 Pro+ (Phantom White, 128 GB)",
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXQ9MHEmVoS0Zl1oHkry2LD4p0OQFqJJcY2A&s",
+        "originalPrice": 29999,
+        "discountPrice": 24999,
+        "offerPercentage": 17,
+        "rating": 4.5,
+        "ratingCount": 3210,
+        "tags": [
+          "108MP Quad Rear Camera",
+          "16MP Front Camera",
+          "16.9 cm (6.67 inch) AMOLED Display"
+        ]
+      },
+      {
+        "id": "8",
+        "name": "Google Pixel 8 Pro (Obsidian, 128 GB)",
+        "imageUrl": "https://www.jbhifi.com.au/cdn/shop/files/778107-Product-0-I-638573702412223281.jpg?v=1723522521",
+        "originalPrice": 89999,
+        "discountPrice": 79999,
+        "offerPercentage": 11,
+        "rating": 4.7,
+        "ratingCount": 4420,
+        "tags": [
+          "50MP Triple Rear Camera",
+          "10.8MP Front Camera",
+          "17.3 cm (6.7 inch) LTPO OLED Display"
+        ]
+      },
+      {
+        "id": "9",
+        "name": "Sony Xperia 1 IV (Black, 256 GB)",
+        "imageUrl": "https://m.media-amazon.com/images/I/31IZYeiNelL._AC_UF1000,1000_QL80_.jpg",
+        "originalPrice": 119990,
+        "discountPrice": 99990,
+        "offerPercentage": 17,
+        "rating": 4.4,
+        "ratingCount": 1865,
+        "tags": [
+          "12MP Triple Rear Camera",
+          "12MP Front Camera",
+          "16.7 cm (6.5 inch) 4K OLED Display"
+        ]
+      }
+    ];
+    
+  
